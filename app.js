1368ef3a028c933ae9e47987259a0c9c9f776105
Jacob Hannah <jacob.hannah164@gmail.com>
2022-06-16 11:03:54 -0400

    added validation to some questions that the user must put in information for

--- app.js
+++ app.js
@@ -12,19 +12,27 @@ const inquirer = require("inquirer");
 
 const promptUser = () => {
     return inquirer.prompt([
-        //question 1
+        //Ask for username
         {
             type: "input",
             name: "name",
-            message: "What is your name?"
+            message: "What is your name?",
+            validate: nameInput => {
+                if (nameInput){
+                    return true;
+                } else {
+                    console.log("Please enter your name!");
+                    return false;
+                }
+            }
         },
-        //question 2
+        //ask for github username
         {
             type: "input",
             name: "github",
             message: "Enter your GitHub Username: "
         },
-        //question 3
+        //ask for user description
         {
             type: "input",
             name: "about",
@@ -44,38 +52,63 @@ const promptProject = portfolioData => {
     =================
     `);
     return inquirer.prompt([
+        //project name
         {
             type: 'input',
             name: 'name',
-            message: 'What is the name of your project?'
+            message: 'What is the name of your project?',
+            validate: nameInput => {
+                if (nameInput){
+                    return true;
+                } else {
+                    console.log("Please enter your project name!");
+                    return false;
+                }
+            }
         },
-
+        //project description
         {
             type: 'input',
             name: 'description',
-            message: 'Provide a description of the project (Required)'
+            message: 'Provide a description of the project (Required)',
+            validate: description => {
+                if (description){
+                    return true;
+                } else {
+                    console.log("Please enter a project description");
+                    return false;
+                }
+            }
         },
-
+        //project language makeup
         {
             type: 'checkbox',
             name: 'languages',
             message: 'What did you build this project with? (Check all that apply)',
             choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
         },
-
+        //project link
         {
             type: 'input',
             name: 'link',
-            message: 'Enter the GitHub link to your project. (Required)'
+            message: 'Enter the GitHub link to your project. (Required)',
+            validate: link => {
+                if (link){
+                    return true;
+                } else {
+                    console.log("Please enter project link!");
+                    return false;
+                }
+            }
         },
-
+        //wether or not the project should be a main focus
         {
             type: 'confirm',
             name: 'feature',
             message: 'Would you like to feature this project?',
             default: false
         },
-
+        //ask if the user would like to input more projects
         {
             type: 'confirm',
             name: 'confirmAddProject',
